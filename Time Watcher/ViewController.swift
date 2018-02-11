088b5d46108c79c74a9d33ff7ce940494bfc5ccf
Lanquemar <lanquemar@gmail.com>
2018-02-11 18:00:14 +0000

    Initial Commit

---
+++ Time Watcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Time Watcher
+//
+//  Created by Adrien Vasseur on 11/02/2018.
+//  Copyright © 2018 Adrien Vasseur. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
